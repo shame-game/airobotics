7d8848b8cb0845a185746ace7ff59f25f1439af5
shame-game <142651893+shame-game@users.noreply.github.com>
2024-06-20 20:05:21 +0700

    f

--- System/Login/index.js
+++ System/Login/index.js
@@ -2,39 +2,39 @@ let url = 'https://script.google.com/macros/s/AKfycbzZLVxj9npEumaJTtKDl24gBahrl_
 
 function Check() {
     let currentUrl = window.location.href;
-    if (currentUrl == 'https://shame-game.github.io/AIR/Login/index.html' || currentUrl == 'https://shame-game.github.io/AIR/Login/' || currentUrl == 'https://shame-game.github.io/AIR/index.html' || currentUrl == 'https://shame-game.github.io/AIR/' || currentUrl == 'https://shame-game.github.io/AIR/Parents/' || currentUrl == 'https://shame-game.github.io/AIR/Parents/index.html') {
+    if (currentUrl == 'https://airobotic.edu.vn/System/Login/index.html' || currentUrl == 'https://airobotic.edu.vn/System/Login/' || currentUrl == 'https://airobotic.edu.vn/System/index.html' || currentUrl == 'https://airobotic.edu.vn/System/' || currentUrl == 'https://airobotic.edu.vn/System/Parents/' || currentUrl == 'https://airobotic.edu.vn/System/Parents/index.html') {
         if (getCookie("vamnaone")) {
             fetch(`${url}?user=${getCookie("vamnaone").split('|')[1]}&token=${getCookie("vamnaone").split('|')[0]}&type=${getCookie('type')}&action=check`, { method: 'Get' })
                 .then(response => response.json())
                 .then((data) => {
                     if (data.status != 'success') {
-                        window.location.href = "https://shame-game.github.io/AIR/Login/"
+                        window.location.href = "https://airobotic.edu.vn/System/Login/"
                     } else {
                         if (data.message.split(' ')[2] == 'parents') {
-                            if (currentUrl == 'https://shame-game.github.io/AIR/Parents/' || currentUrl == 'https://shame-game.github.io/AIR/Parents/index.html') {
+                            if (currentUrl == 'https://airobotic.edu.vn/System/Parents/' || currentUrl == 'https://airobotic.edu.vn/System/Parents/index.html') {
                             } else {
-                                window.location.href = 'https://shame-game.github.io/AIR/Parents/'
+                                window.location.href = 'https://airobotic.edu.vn/System/Parents/'
                             }
                         }
                         else if (data.message.split(' ')[2] == 'teacher') {
-                            if (currentUrl == 'https://shame-game.github.io/AIR/index.html' || currentUrl == 'https://shame-game.github.io/AIR/') {
+                            if (currentUrl == 'https://airobotic.edu.vn/System/index.html' || currentUrl == 'https://airobotic.edu.vn/System/') {
                             } else {
-                                window.location.href = 'https://shame-game.github.io/AIR/'
+                                window.location.href = 'https://airobotic.edu.vn/System/'
                             }
                         }
                     }
                 })
                 .catch(error => alert('Lỗi: ' + error));
         } else {
-            if (currentUrl == 'https://shame-game.github.io/AIR/Login/index.html' || currentUrl == 'https://shame-game.github.io/AIR/Login/') {
+            if (currentUrl == 'https://airobotic.edu.vn/System/Login/index.html' || currentUrl == 'https://airobotic.edu.vn/System/Login/') {
             } else {
-                window.location.href = "https://shame-game.github.io/AIR/Login/"
+                window.location.href = "https://airobotic.edu.vn/System/Login/"
             }
         }
     }
 }
 
-Check()
+/*Check()*/
 
 function getCookie(name) {
     var cookieName = name + "=";
@@ -57,7 +57,7 @@ if (document.querySelector('.logout')) {
         document.cookie = 'vamnaone' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         document.cookie = 'type' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         document.cookie = 'id' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        window.location.href = "https://shame-game.github.io/AIR/Login/"
+        window.location.href = "https://airobotic.edu.vn/System/Login/"
     }
 }
 
